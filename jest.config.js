24f2b1f247f6a664ce9e744a7499c938d26ee3a3
naughtLdy <naughtLdy@users.noreply.github.com>
2019-09-30 12:06:14 +0900

    chore: initial commit

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^~/(.*)$': '<rootDir>/src/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: ['js', 'vue', 'json'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/components/**/*.vue',
+    '<rootDir>/src/pages/**/*.vue'
+  ]
+}
